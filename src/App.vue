594fcc3796edd1f99f6c0cd282116f8b5386935b
vlevin <vlevin@msn.cn>
2021-07-30 08:37:21 +0800

    优化：更新依赖、ElementPlus使用新版汉化方式

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,13 @@
 <template>
-  <router-view />
+  <el-config-provider :locale="locale">
+    <router-view />
+  </el-config-provider>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+import { ElConfigProvider } from 'element-plus'
+
+import zhCn from 'element-plus/lib/locale/lang/zh-cn'
+const locale = ref(zhCn)
+</script>
--- src/main.js
+++ src/main.js
@@ -6,16 +6,7 @@ import { fetchData } from 'S/utils/request.js'
 import { eventHub } from 'S/utils/eventHub.js'
 
 // vlevin @ 2021-07-05 ElementPlus
-import { locale, ElMessage } from 'element-plus'
-// vlevin @ 2021-04-15 ElementPlus汉化
-import lang from 'element-plus/lib/locale/lang/zh-cn'
-// vlevin @ 2021-04-26 ---以下内容勿动（开始）---
-// vlevin @ 2021-04-26 原因：vite或element-plus存在bug，locale在开发模式下是个函数，打包之后是个对象，导致打包后的代码无法执行
-// vlevin @ 2021-04-26 此处主动判断环境并手动修复，如果日后官方修复了bug，可直接使用locale(lang)
-import { isLocal } from './utils/url.js'
-const localize = isLocal() ? locale : locale.use
-localize(lang)
-// vlevin @ 2021-04-26 ---以上内容勿动（结束）---
+import { ElMessage } from 'element-plus'
 
 // vlevin @ 2021-04-15 样式
 import 'element-plus/lib/theme-chalk/index.css'
